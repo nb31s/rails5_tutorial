b1e845193253afbe4feec9855b0570d18a5c0b5f
nbnb-31 <yy.r9pk@gmail.com>
2017-01-06 19:14:40 +0900

    Add ArticlesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :articles
 
   root 'welcome#index'
-  get 'welcome/index'
+# get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
